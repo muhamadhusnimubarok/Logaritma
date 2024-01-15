daed1f46bd3900410c9810f8b270eaabbbbf2929
Dynoxxx <muhamadhusnimubarok@smkwikrama.sch>
2024-01-15 11:00:30 +0700

    Tahun Kabisat

---
+++ Kabisat/Kabisat.js
@@ -0,0 +1,51 @@
+// for ( let tahun=1600; tahun <=2404; tahun+=4) {
+//         if (tahun % 4 >= 1 ) {
+//             console.log (' Bukan Tahun Kabisat',tahun );
+//         } else if ( tahun%100>=1) {
+//             console.log (' Tahun Kabisat', tahun);
+//         } else if ( tahun%400>=1) {
+//             console.log (' Bukan Tahun Kabisat', tahun)
+//         } else {
+//             console.log ('Tahun Kabisat', tahun)
+//         }
+//     }
+
+ //////////////////////////////////////////////////////////////////////////////////////////
+ 
+//  let tahun= 1564;
+
+//  while (tahun <=2404 ) {
+//     tahun+=4
+// if (tahun % 4 >= 1 ) {
+//     console.log (' Bukan Tahun Kabisat',tahun );
+// } else if ( tahun%100>=1) {
+//     console.log (' Tahun Kabisat', tahun);
+// } else if ( tahun%400>=1) {
+//     console.log (' Bukan Tahun Kabisat', tahun)
+// } else {
+//     console.log ('Tahun Kabisat', tahun)
+// }
+//  };
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+let tahun = 1564;
+
+do {
+    if (tahun % 4 >= 1 ) {
+    console.log (' Bukan Tahun Kabisat',tahun );
+} else if ( tahun%100>=1) {
+    console.log (' Tahun Kabisat', tahun);
+} else if ( tahun%400>=1) {
+    console.log (' Bukan Tahun Kabisat', tahun)
+} else {
+    console.log ('Tahun Kabisat', tahun)
+}
+tahun+=4
+}while ( tahun <=2404);
+
+    
+    
+    
+
+
