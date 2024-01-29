2c4a96b55b5bda8bf9f935cce8c808ba9daa1277
Dynoxxx <muhamadhusnimubarok@smkwikrama.sch>
2024-01-29 08:48:00 +0700

    LKPD

---
+++ LKPD2/wii.js
@@ -0,0 +1,47 @@
+// let nama = prompt('masukkan nama karyawan');
+//         let gaji_pokok = prompt ( 'gaji pokok');
+//         let tunjangan = gaji_pokok * 20/100;
+//         let pajak = 15/100 * (gaji_pokok + tunjangan);
+//         let gaji_bersih = gaji_pokok + tunjangan - pajak;
+
+//         console.log(nama + " gaji pokok mu adalah " + gaji_pokok)
+//         console.log("lalu tunjanganmu bernilai " + tunjangan)
+//         console.log("pajak mu adalah " + pajak)
+//         console.log("hasil dari gaji bersihmu adalah " + gaji_bersih)
+
+
+
+/////////////////////////////////////
+
+// let jam = parseInt(prompt("Masukkan jam:"));
+// let menit = parseInt(prompt("Masukkan total menit:"));
+// let detik  = parseInt(prompt("Masukkan total detik:"));
+// let total = jam * 3600 + menit * 60 + detik;
+
+// console.log ('total detik', total);
+
+
+
+//////////////////////////////////////////////////////////
+
+
+
+// let totalDetik = parseInt(prompt("Masukkan total detik:"));
+
+
+// let jam = Math.floor(totalDetik / 3600);
+// let sisaDetik1 = totalDetik % 3600;
+// let menit = Math.floor(sisaDetik1 / 60);
+// let detik = sisaDetik1 % 60;
+
+
+// console.log(`${totalDetik} detik sama dengan ${jam} jam ${menit} menit ${detik} detik.`);
+
+
+/////////////////////////LKPD 3///////////////////////////////////
+
+
+
+
+
+
---
+++ LKPD3/lkpd3.js
@@ -0,0 +1,52 @@
+// let berat = parseFloat(prompt("Masukkan berat jeruk (gram):"));
+
+
+// let harga = 500;
+// let hargaSebelum= (berat/ 1000) * harga;
+
+
+// let diskon = 0.05 * hargaSebelum;
+
+
+// let HargaSetelah = hargaSebelum - diskon;
+
+
+// console.log(`Total Harga Sebelum Diskon: ${hargaSebelum} rupiah`);
+// console.log(`Diskon: ${diskon} rupiah`);
+// console.log(`Total Harga Setelah Diskon: ${HargaSetelah} rupiah`);
+
+
+////////////////////////////////////////////////////////////////////
+
+
+// let inputBilangan = parseInt(prompt("Masukkan bilangan bulat:"));
+
+
+// let satuan = parseInt(inputBilangan.toString().charAt(inputBilangan.toString().length - 1)) || 0;
+// let puluhan = parseInt(inputBilangan.toString().charAt(inputBilangan.toString().length - 2)) || 0;
+// let ratusan = parseInt(inputBilangan.toString().charAt(inputBilangan.toString().length - 3)) || 0;
+
+
+// console.log(`Satuan: ${satuan}`);
+// console.log(`Puluhan: ${puluhan}`);
+// console.log(`Ratusan: ${ratusan}`);
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+  
+// let suhuFahrenheit = parseFloat(prompt("Masukkan suhu dalam derajat Fahrenheit:"));
+
+// if (!isNaN(suhuFahrenheit)) {
+
+//     let suhuCelsius = (suhuFahrenheit - 32) * 5/9;
+
+//     if (suhuCelsius > 30) {
+//         console.log("Cuaca saat ini: Panas");
+//     } else if (suhuCelsius < 10) {
+//         console.log("Cuaca saat ini: Dingin");
+//     } else {
+//         console.log("Cuaca saat ini: Normal");
+//     }
+// } else {
+//     console.log("Masukan tidak valid. Silakan masukkan angka.");
+// }
